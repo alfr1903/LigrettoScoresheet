fac20e5712339298df8a5d1bb2760e8a989b5aef
Alexander Fredheim <alfr@netlight.com>
2023-07-11 13:50:52 +0200

    initial commit

---
+++ app/src/main/java/no/fredheim/ligretto_scoresheet/LigrettoScreen.kt
@@ -0,0 +1,18 @@
+package no.fredheim.ligretto_scoresheet
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+enum class LigrettoScreen {
+    Welcome,
+    Players,
+    PlayerRoundScore,
+    Results
+}
+
+@Composable
+fun LigrettoApp(
+    modifier: Modifier
+) {
+
+}
