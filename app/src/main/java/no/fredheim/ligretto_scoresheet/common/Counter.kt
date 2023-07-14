9b817638d1db01bab14682090daedfac1629a3ea
Alexander Fredheim <alfr@netlight.com>
2023-07-14 21:34:21 +0200

    made Counter composable look decent

--- app/src/main/java/no/fredheim/ligretto_scoresheet/common/Counter.kt
+++ app/src/main/java/no/fredheim/ligretto_scoresheet/common/Counter.kt
@@ -2,7 +2,9 @@ package no.fredheim.ligretto_scoresheet.common
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,8 +23,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import no.fredheim.ligretto_scoresheet.R
+import no.fredheim.ligretto_scoresheet.service.CalculationService
+import no.fredheim.ligretto_scoresheet.ui.theme.LigrettoScoresheetTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,7 +44,7 @@ fun Counter(
             painter = painterResource(id = R.drawable.subtract),
             contentDescription = stringResource(R.string.subtract),
             modifier = Modifier.clickable {
-                onValueChange(value.toIntOrNull()?.dec().toString())
+                onValueChange(CalculationService.decrement(value))
             }
         )
         TextField(
@@ -58,14 +63,33 @@ fun Counter(
             shape = RoundedCornerShape(16.dp),
             modifier = Modifier
                 .padding(16.dp)
-                .width(60.dp)
+                .width(60.dp),
+            placeholder = {
+                Text(
+                    text = "0",
+                    textAlign = TextAlign.Center,
+                    style = LocalTextStyle.current.copy(color = Color.Gray),
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
         )
         Image(
             painter = painterResource(id = R.drawable.add),
             contentDescription = stringResource(R.string.add),
             modifier = Modifier.clickable {
-                onValueChange(value.toIntOrNull()?.inc().toString())
+                onValueChange(CalculationService.increment(value))
             }
         )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = "id:pixel_4"
+)
+@Composable
+fun CounterPreview() {
+    LigrettoScoresheetTheme {
+        Counter(value = "", onValueChange = {})
     }
 }
--- app/src/main/java/no/fredheim/ligretto_scoresheet/service/CalculationService.kt
+++ app/src/main/java/no/fredheim/ligretto_scoresheet/service/CalculationService.kt
@@ -9,4 +9,12 @@ object CalculationService {
             CardType.Center -> numCards
             CardType.Ligretto -> numCards * -2
         }
+
+    fun increment(value: String): String =
+        if (!value.isDigitsOnly()) "1" else value.toInt().inc().toString()
+
+    fun decrement(value: String): String =
+        if (!value.isDigitsOnly() || value == "1") "" else value.toInt().dec().toString()
+
+    private fun String.isDigitsOnly() = all(Char::isDigit) && isNotEmpty()
 }
